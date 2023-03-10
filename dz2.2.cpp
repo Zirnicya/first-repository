3ddf6494421f21ddcde50d0e68f52d3b5d322fa5
zirnicya <zirnicya@gmail.com>
2023-03-11 00:43:48 +0600

    added dz

---
+++ dz2.2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+int main ()
+{
+int a,b;
+printf (a=);
+scanf (%i,&a);
+printf (b=);
+scanf (%i,&b);
+if (a>b);
+{
+printf (A bolshe);
+}
+else
+{
+if (a==b);
+printf (A=B);
+}
+else
+{
+printf (B bolshe);
+}
+}
+
